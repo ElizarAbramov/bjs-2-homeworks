a4db60f4e85c25a778eea9df425c6e78b1c16c6f
Elizar Abramov <nezha2077@mail.ru>
2022-07-26 18:36:12 +0700

    Prototype were added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let first = new Student("Kirill", "male", 22);
+let second = new Student("Polina", "female", 21);
+let third = new Student("Ivan", "male", 22);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [...mark];
+  } else {
+    this.marks.push(mark);
+  }
+
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (e of this.marks) {
+    sum += e;
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+
+  if (this.excluded === undefined) {
+    this.excluded = reason;
+    delete this.marks;
+    delete this.subject;
+  }
+}
